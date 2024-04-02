79062cb048d5389dad7ffd89b4d29c420b434da8
luisesales <duduosales@hotmail.com>
2024-04-01 21:56:49 -0300

    adding rotate ship and attack functions untested

--- Scripts/Board.gd
+++ Scripts/Board.gd
@@ -11,10 +11,11 @@ func _ready():
 	for x in gameController.gridSize:
 		for y in gameController.gridSize:
 			gameController.playerBoard[str(Vector2(x,y))] = {
-					"type": "default" ,
+					"type": 0 ,
 					"x" : x,
 					"y" : y,
-					"vertical" : false
+					"vertical" : false,
+					"boat" : -1
 					
 			}
 			set_cell(0, Vector2(x,y), 2 , Vector2i(0,0), 0) 
@@ -24,20 +25,42 @@ func _ready():
 	for x in range(gameController.gridSize + gameController.gridDistance, 2*gameController.gridSize+gameController.gridDistance) : 
 		for y in gameController.gridSize: 
 			gameController.enemyBoard[str(Vector2(x,y))] = {
-					"type": "default" ,
+					"type": 0 ,
 					"x" : x,
 					"y" : y,
-					"vertical" : false
+					"vertical" : false,
+					"boat" : -1
 					
 			}
 			set_cell(0, Vector2(x,y), 2 , Vector2i(0,0), 0) 
 			set_cell(1, Vector2(x,y), 8 , Vector2i(0,0), 0) 
 
-func attack(tile):
-	pass
+func locateShip(tile, board): 
+	var ship = {}
+	for boardTile in board :
+		if boardTile["boat"] == board[str(tile)]["boat"] : 
+			ship[str(Vector2(board[str(tile)]["x"],board[str(tile)]["y"]))] = boardTile
+	return ship
+	##return  {piece : data for piece, data in board.items() if data.get("boat") == board[str(tile)]["boat"]}
 
-func rotateShip(tile):
-	pass
+func attack(tile, board):
+	if board[str(tile)]["type"] > 1 : 
+		if board[str(tile)]["type"] == 2 : 
+			board[str(tile)]["type"] = 0
+		else :
+			board[str(tile)]["type"] = 1		
+		set_cell(0, Vector2(board[str(tile)]["x"],board[str(tile)]["y"]), board[str(tile)]["type"] , Vector2i(0,0), 0) 	
+
+func rotateShip(tile, board):
+	if board[str(tile)]["type"] > 2 :		
+		var ship = locateShip(tile, board)
+		if 	board[str(tile)]["vertical"] :
+			for n in range (1, gameController.shipSize[ship[0]["type"]]) : 
+				if board[str(Vector2(board[str(tile)]["x"]+n, board[str(tile)]["y"]))]["type"] > 2 :
+					return
+			for n in range (1, gameController.shipSize[ship[0]["type"]]) : 
+				set_cell(0, Vector2(board[str(tile)]["x"]+n,board[str(tile)]["y"]), board[str(tile)]["type"] , Vector2i(0,0), 0) 	
+				set_cell(0, Vector2(board[str(tile)]["x"],board[str(tile)]["y"]+n), 2 , Vector2i(0,0), 0) 	
 
 func moveShip(tile): 
 	pass
@@ -45,6 +68,10 @@ func moveShip(tile):
 func selectTile(tile):	
 		set_cell(1, tile, 0, Vector2i(0,0), 0)
 		aux = tile
+		
+func setupGame():
+	gameController.playerBoard.clear()
+	gameController.enemyBoard.clear()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):		
@@ -56,8 +83,8 @@ func _process(delta):
 		if(Input.is_action_just_pressed("mb_left")):
 			moveShip(tile)
 		if(Input.is_action_just_pressed("mb_right")):
-			rotateShip(tile)
+			rotateShip(tile, gameController.playerBoard)
 	if gameController.enemyBoard.has(str(tile)):
 		selectTile(tile)	
 		if(Input.is_action_just_pressed("mb_left")):
-			attack(tile)
+			attack(tile, gameController.enemyBoard)
--- Scripts/GameController.gd
+++ Scripts/GameController.gd
@@ -9,3 +9,10 @@ const gridDistance = 2
 @export var playerBoard = {} 
 @export var enemyBoard = {}
 
+const shipSize = {
+	3 : 4,
+	4 : 5,
+	5 : 3,
+	6 : 2,
+	7 : 3
+}
